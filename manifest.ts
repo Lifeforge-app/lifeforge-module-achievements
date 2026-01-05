a9c5462c7e87c4944eebe277d44f359b8f261b07
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 12:01:35 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,10 +2,7 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Achievements',
-  icon: 'tabler:award',
   routes: {
     '/': lazy(() => import('@'))
-  },
-  category: 'Lifestyle'
+  }
 } satisfies ModuleConfig
