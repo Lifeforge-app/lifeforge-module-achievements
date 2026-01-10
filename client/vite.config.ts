5e311c34c0cc8b82e55dcb2863b718b38c9364f1
Melvin Chia <melvinchia623600@gmail.com>
2026-01-10 09:42:44 +0800

    feat: update to latest version from forgistry

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,50 +1,71 @@
 import federation from '@originjs/vite-plugin-federation'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 import path from 'node:path'
 import { defineConfig } from 'vite'
 
+import pkg from '../package.json'
+
+dotenv.config({ path: '../../../env/.env.local' })
+
+const apiHost = process.env.VITE_API_HOST
+
+if (!apiHost) {
+  throw new Error('VITE_API_HOST is not defined')
+}
+
+const moduleName = pkg.name.replace('@lifeforge/', '')
+
 export default defineConfig({
+  base: `${apiHost}/modules/${moduleName}/`,
   plugins: [
     react(),
+    tailwindcss(),
     federation({
-      name: 'lifeforge_achievements',
+      name: moduleName,
       filename: 'remoteEntry.js',
       exposes: {
-        './Module': './index.tsx'
+        './Manifest': './manifest.ts'
       },
-      shared: [
-        'react',
-        'react-dom',
-        {
-          shared: {
-            version: 'workspace:*',
-            generate: false,
-            shareScope: 'lifeforge'
-          },
-          'lifeforge-ui': {
-            version: 'workspace:*',
-            generate: false,
-            shareScope: 'lifeforge'
-          },
-          'react-i18next': {
-            generate: false
-          },
-          i18next: {
-            generate: false
-          }
+      shared: {
+        react: {
+          generate: false
         },
-        '@tanstack/react-query'
-      ]
+        'react-dom': {
+          generate: false
+        },
+        shared: {
+          generate: false
+        },
+        'lifeforge-ui': {
+          generate: false
+        },
+        'react-i18next': {
+          generate: false
+        },
+        i18next: {
+          generate: false
+        },
+        '@tanstack/react-query': {
+          generate: false
+        }
+      }
     })
   ],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './'),
-      '@server': path.resolve(__dirname, '../../../server/src')
-    }
+    alias: [
+      {
+        find: '@server',
+        replacement: path.resolve(__dirname, '../../../server/src')
+      },
+      { find: /^@\/(.*)$/, replacement: path.resolve(__dirname, './src/$1') },
+      { find: /^@$/, replacement: path.resolve(__dirname, './src/index') }
+    ]
   },
   build: {
     target: 'esnext',
-    minify: false
+    minify: true,
+    modulePreload: false
   }
 })
